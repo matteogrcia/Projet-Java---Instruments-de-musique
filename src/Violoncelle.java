acb32a7fbc626186ae1f407caedaf7f4cb202b70
CHAIMA <chaimayedes25@gmail.com>
2026-01-29 17:00:17 +0100

    Violoncelle

--- src/Violoncelle.java
+++ src/Violoncelle.java
@@ -1,2 +1,20 @@
 public class Violoncelle extends Corde{
+
+    private String type;
+    public Violoncelle(int id, String nom, String marque, double prix, int nbCordes, String type) {
+        super(id, nom, marque, prix, nbCordes);
+        this.type = type;
+    }
+    public Violoncelle()  {
+        super(0, "Inconnu", "Inconnue", 0.0, 0);
+    }
+
+    @Override
+    public void jouer() {
+        System.out.println("Le saxophone " + type + " " + nom + " joue une mélodie douce forte.");
+    }
+
+    public String getType() {
+        return type;
+    }
 }
